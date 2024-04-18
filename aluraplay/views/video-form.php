40a77d0e9fddfde07231de3f5824b5b2937e8333
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-04-17 21:34:21 -0300

    commit 5

--- aluraplay/views/video-form.php
+++ aluraplay/views/video-form.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/inicio-html.php';
 ?>
 
 <main class="container">
-  <form class="container__formulario" method="post">
+  <form enctype="multipart/form-data" class="container__formulario" method="post">
     <h2 class="formulario__titulo">Envie um vídeo!</h2>
     <div class="formulario__campo">
       <label class="campo__etiqueta" for="url">Link embed</label>
@@ -17,6 +17,11 @@ require_once __DIR__ . '/inicio-html.php';
     <div class="formulario__campo">
       <label class="campo__etiqueta" for="titulo">Titulo do vídeo</label>
       <input name="titulo" value="<?= $video?->title; ?>" class="campo__escrita" required placeholder="Neste campo, dê o nome do vídeo" id='titulo' />
+    </div>
+
+    <div class="formulario__campo">
+      <label class="campo__etiqueta" for="image">Imagem do vídeo</label>
+      <input name="image" accept="image/*" type="file" class="campo__escrita" id='image' />
     </div>
 
     <input class="formulario__botao" type="submit" value="Enviar" />
